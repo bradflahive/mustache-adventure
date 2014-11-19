1140f083d05161316d3d558bc00c87df60930b80
Jon Nyman <nymanjon@gmail.com>
2014-11-19 00:10:06 -0700

    updated readme. added schema.

--- database.sql
+++ database.sql
@@ -1,24 +1,10 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10
--- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Oct 30, 2014 at 09:34 PM
--- Server version: 5.6.15
--- PHP Version: 5.4.30
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
+-- Database: `mustache_adventure_db`
 --
--- Database: `bootcamp_mvc`
---
+
+DROP DATABASE mustache_adventure_db;
+CREATE DATABASE mustache_adventure_db;
+USE mustache_adventure_db;
 
 -- --------------------------------------------------------
 
@@ -26,15 +12,14 @@ SET time_zone = "+00:00";
 -- Table structure for table `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(15) NOT NULL DEFAULT '',
-  `last_name` varchar(15) NOT NULL DEFAULT '',
+  `user_name` varchar(15) NOT NULL DEFAULT '',
   `email` varchar(100) NOT NULL DEFAULT '',
   `password` varchar(32) NOT NULL DEFAULT '',
   `datetime_added` datetime NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+);
 
 --
 -- Dumping data for table `user`
@@ -44,6 +29,59 @@ INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `email`, `password`, `
 (1, 'Lindsey', 'Jones', 'lindsey@jones.com', 'abc123', '2014-10-08 00:00:00'),
 (2, 'Dave', 'Smith', 'dave@smith.com', 'abc123', '2014-10-23 00:00:00');
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- 
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment_type` (
+    `comment_type_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `name` varchar(15),
+    `description` varchar(255),
+    PRIMARY KEY (`comment_type_id`)
+);
+
+-- 
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+    `comment_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `comment_to_user_id` int(10) unsigned NOT NULL,
+    `comment_from_user_id` int(10) unsigned NOT NULL,
+    `comment_type_id` int(10) unsigned NOT NULL,
+    `message` varchar(255) NOT NULL DEFAULT 'man up!',
+    `timestamp` timestamp,
+    PRIMARY KEY (`comment_id`),
+    FOREIGN KEY (`comment_to_user_id`) REFERENCES user (user_id),
+    FOREIGN KEY (`comment_from_user_id`) REFERENCES user (user_id),
+    FOREIGN KEY (`comment_type_id`) REFERENCES comment_type (comment_type_id)
+);
+
+-- 
+-- Table structure for table `man_point`
+--
+
+CREATE TABLE `man_point` (
+    `give_user_id` int(10) unsigned NOT NULL,
+    `receive_user_id` int(10) unsigned NOT NULL,
+    `comment_id` int(10) unsigned NOT NULL,
+    `points` int NOT NULL,
+    `timestamp` timestamp,
+    FOREIGN KEY (give_user_id) REFERENCES user (user_id),
+    FOREIGN KEY (receive_user_id) REFERENCES user (user_id),
+    FOREIGN KEY (comment_id) REFERENCES comment (comment_id)
+);
+
+-- 
+-- Table structure for table `give_point`
+--
+
+CREATE TABLE `give_point` (
+    `give_point_id` int(10) unsigned NOT NULL,
+    `user_id` int(10) unsigned NOT NULL,
+    `points` int NOT NULL,
+    `timestamp` timestamp,
+    `description` varchar(255),
+    PRIMARY KEY (`give_point_id`),
+    FOREIGN KEY (`user_id`) REFERENCES user (user_id)
+);
