de4d2012671e9aaaaa96c06d0333400727f8df39
Nathan Atkinson <nathanatkinson@github.com>
2014-11-19 15:51:16 -0700

    Updated Validators

--- app/utilities/validators/gender_validator.class.php
+++ app/utilities/validators/gender_validator.class.php
@@ -3,10 +3,10 @@
 
 	class GenderValidator extends Validator {
 		protected function validateParam($value) {
-			if(preg_match('/m|f|M|F/',$value) == 0) {
+			return (preg_match('/m|f|M|F/',$value) == 0); /*{
 				throw new ValidationException('Invalid indicator for gender: ');
 			} else {
 				return $value;
-			}
+			}*/
 		}
 	}
--- app/utilities/validators/number_validator.class.php
+++ app/utilities/validators/number_validator.class.php
@@ -5,10 +5,10 @@
 class NumberValidator extends Validator {
 
 	protected function validateParam($value) {
-		if(!is_numeric($value)) {
+		return (is_numeric($value)); /*{
 			throw new ValidationException('Invalid number: ');
 		} else {
 			return $value;
-		}
+		}*/
 	}
 }
