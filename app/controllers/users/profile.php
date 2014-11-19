0b9b835f560fd91c59270179ac3a0e430be4099b
Nathan Atkinson <nathanatkinson@github.com>
2014-11-19 11:28:27 -0700

    Added blank profile.php

---
+++ app/controllers/users/profile.php
@@ -0,0 +1,18 @@
+<?php
+
+// Controller
+class Controller extends AppController {
+	protected function init() {
+
+		//need to build the profile page
+
+	}
+
+}
+$controller = new Controller();
+
+// Extract Main Controler Vars
+extract($controller->view->vars);
+
+?>
+
