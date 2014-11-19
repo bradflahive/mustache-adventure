5a434332b7c1701c858da71e3972abde6f1ebd35
Jon Nyman <nymanjon@gmail.com>
2014-11-19 16:48:07 -0700

    database is working and has sample data. comment box is functional.

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 --
--- Database: `mustache_adventure_db`
+-- Database: mustache_adventure_db
 --
 
 DROP DATABASE mustache_adventure_db;
@@ -9,40 +9,40 @@ USE mustache_adventure_db;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table user
 --
 
-CREATE TABLE `user` (
-  `user_id` int unsigned NOT NULL AUTO_INCREMENT,
-  `user_name` varchar(15) NOT NULL DEFAULT '',
-  `email` varchar(100) NOT NULL DEFAULT '',
+CREATE TABLE user (
+  user_id int unsigned NOT NULL AUTO_INCREMENT,
+  user_name varchar(15) NOT NULL DEFAULT '',
+  email varchar(100) NOT NULL DEFAULT '',
   `password` varchar(32) NOT NULL DEFAULT '',
-  `datetime_added` timestamp NOT NULL,
-  PRIMARY KEY (`user_id`)
+  `timestamp` timestamp NOT NULL,
+  PRIMARY KEY (user_id)
 );
 
 
 -- 
--- Table structure for table `comment`
+-- Table structure for table comment
 --
 
-CREATE TABLE `comment` (
-    `comment_id` int unsigned NOT NULL AUTO_INCREMENT,
-    `user_id` int unsigned NOT NULL,
-    `message` varchar(255) NOT NULL DEFAULT 'man up!',
-    `timestamp` timestamp,
-    PRIMARY KEY (`comment_id`),
-    FOREIGN KEY (`user_id`) REFERENCES user (user_id),
+CREATE TABLE comment (
+    comment_id int unsigned NOT NULL AUTO_INCREMENT,
+    user_id int unsigned NOT NULL,
+    message varchar(255) NOT NULL DEFAULT 'man up!',
+    `timestamp` timestamp NOT NULL,
+    PRIMARY KEY (comment_id),
+    FOREIGN KEY (user_id) REFERENCES user(user_id)
 );
 
 -- 
--- Table structure for table `man_point`
+-- Table structure for table man_point
 --
 
-CREATE TABLE `man_point` (
-    `user_id` int unsigned NOT NULL,
-    `comment_id` int unsigned NOT NULL,
-    `points` int NOT NULL,
+CREATE TABLE man_point (
+    user_id int unsigned NOT NULL,
+    comment_id int unsigned NOT NULL,
+    points int NOT NULL,
     `timestamp` timestamp,
     FOREIGN KEY (user_id) REFERENCES user (user_id),
     FOREIGN KEY (comment_id) REFERENCES comment (comment_id)
@@ -51,9 +51,24 @@ CREATE TABLE `man_point` (
 -- --------------------------------------------------------
 
 --
--- Dumping data for table `user`
+-- Dumping data for table user
 --
 
-INSERT INTO `user` (`user_id`, `first_name`, `last_name`, `email`, `password`, `datetime_added`) VALUES
-(1, 'Lindsey', 'Jones', 'lindsey@jones.com', 'abc123', '2014-10-08 00:00:00'),
-(2, 'Dave', 'Smith', 'dave@smith.com', 'abc123', '2014-10-23 00:00:00');
+INSERT INTO 
+    user (user_name, email, `password`)
+    VALUES 
+        ('LindseyGirl', 'lindsey@jones.com', PASSWORD('abc123')),
+        ('Smithy', 'dave@smith.com', PASSWORD('abc124'));
+
+INSERT INTO
+    comment (user_id, message)
+    VALUES
+        (1, 'I love manly men!'),
+        (1, 'How many points can a WOman get on this forum!'),
+        (2, 'Scaled mount everest in a speedo...'),
+        (2, 'Ate steel for breakfast this morning');
+
+INSERT INTO
+    man_point (user_id, comment_id, points)
+    VALUES
+        (1, 3, 5), (1, 4, 5), (1, 1, 5), (2, 1, -5), (2, 2, -5);
