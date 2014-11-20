98ea686fb068e781637a1316c7f053236ea5d3a5
Mark Ragno <mark@ragno76.com>
2014-11-20 15:09:05 -0700

    modified login controller, corrected app_settings, and deleted create_user file and router

--- app/app_settings.php
+++ app/app_settings.php
@@ -11,7 +11,7 @@ define('LIVE_SITE', FALSE);
   Database
 *****************************************/
 
-define('DB_HOST', 'mustache-adventure');
+define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'mustache-adventure');
+define('DB_NAME', 'mustache_adventure_db');
