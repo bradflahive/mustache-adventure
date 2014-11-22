ba564a01c59e67b9db1708c3c12b51d83855a5f4
Nathan Atkinson <nathanatkinson@github.com>
2014-11-22 15:36:28 -0700

    Created controller to add points to DB upon request

--- app/controllers/update_points.php
+++ app/controllers/update_points.php
@@ -12,13 +12,16 @@
 	protected function init() {
 
 		// if(isset($_POST['points_awarded']) == 1) {
-			$points_awarded = $_POST['points_awarded'];
-			$points_awarded = $_POST['user_id'];
-			$points_awarded = $_POST['comment_id'];
-			
+			$input['points'] = $_POST['points'];
+			$input['user_id'] = $_POST['user_id'];
+			$input['comment_id'] = $_POST['comment_id'];
 
-			$this->view['points_awarded'] = json_encode($points_awarded);
+			Comment::givePoints($input);
+
+			$this->view['points'] = json_encode($points_awarded);
 			
+			// TODO  need to fix so that null isn't returned
+
 		// In the case of the Ajax Controller, the view is an array
 		// which can can be accessed as follows. This array will be
 		// converted to JSON when this script ends and sent to the client
