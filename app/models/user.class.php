7ad151e8d61e04503b6edc5c8b6e40c1caf4d8e2
Jon Nyman <nymanjon@gmail.com>
2014-11-23 07:58:53 -0700

    started work on properly doing login for new user

--- app/models/user.class.php
+++ app/models/user.class.php
@@ -62,13 +62,20 @@ sql;
 		// Prepare SQL Values
         $cleanedInput = $this->cleanInput(
             ['user_name', 'email', 'password'],
-            $input
+            $input, ['password']
         );
 
         if (is_string($cleanedInput)) return null;
 
+        $passwordInsert =<<<sql
+        INSERT INTO 
+        user (user_name, email, `password`)
+        VALUES ({$cleanedInput['user_name']}, {$cleanedInput['email']},
+        PASSWORD(CONCAT({$cleanedInput['user_name']}, {$cleanedInput['password']})));
+sql;
+
 		// Insert
-		$results = db::insert('user', $cleanedInput);
+		$results = db::execute('user', $cleanedInput);
 		
 		// Return the Insert ID
 		return $results->insert_id;
