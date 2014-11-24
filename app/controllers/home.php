7b8bbdc3f587f481ce3a03cac8fd7638ca6fc00e
bradflahive <arizona.flahive@gmail.com>
2014-11-24 15:26:49 -0700

    login

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -16,8 +16,8 @@ extract($controller->view->vars);
 
         <h3>LOGIN</h3>
         <form action="/login" method="POST" class="login-form">
-            <input type="text" name="user_name" title="-Username-">
-            <input type="password" name="password" data-exp-name="password" title="-Password-">
+            <input type="text" name="user_name" title="Username">
+            <input type="password" name="password" data-exp-name="password" title="Password">
             <input type="email" name="email" data-exp-name="email" title="-E-mail-">
             <br>
             <button class="sign-up">sign up</button>
