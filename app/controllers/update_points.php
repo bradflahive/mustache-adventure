e56f06a727c23ca95ef358bf47e28f48416cbe7a
Jon Nyman <nymanjon@gmail.com>
2014-11-24 19:18:31 -0700

    can't remember what I did

--- app/controllers/update_points.php
+++ app/controllers/update_points.php
@@ -5,28 +5,18 @@
  */
  class Controller extends AjaxController {
 
-
-	/**
-	 * The view for this controller will be an array and will be
-	 * converted to JSON upon render
-	 */
-	// can't pass an array called input because base controller init takes no params
 	protected function init() {
 
-			$total = (new Comment())->givePoints($_POST);
+        if ($_POST['comment_id']) {
 
-			//need to get properly working
-			// TODO  need to fix so that null isn't returned
-			// $this->view['points'] = json_encode($points);
-			$this->view['points'] = $total;
-			
+            $total = (new Comment($_POST['comment_id']))->givePoints($_POST);
+            $this->view['points'] = $total;
 
-		// In the case of the Ajax Controller, the view is an array
-		// which can can be accessed as follows. This array will be
-		// converted to JSON when this script ends and sent to the client
-		// automatically
-	}
-
+        }
+        else {
+            $this->view['message'] = 'No comment ID provided';
+        }
+    }
 
 }
 $controller = new Controller();
