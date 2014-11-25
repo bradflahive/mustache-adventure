f269faf8b987420fd574074df8041e8fd1f46e5a
bradflahive <arizona.flahive@gmail.com>
2014-11-25 13:30:36 -0700

    getVotes function in comment.class

--- app/models/comment.class.php
+++ app/models/comment.class.php
@@ -75,7 +75,7 @@ class Comment extends CustomModel {
 
     // return all comments 
     public static function getAll() {
-        $getPointsFromSql =<<<sql
+        $getComments =<<<sql
         SELECT
             comment_id,
             message,
@@ -89,7 +89,7 @@ class Comment extends CustomModel {
 sql;
 
         //returns the comments
-        return db::execute($getPointsFromSql);
+        return db::execute($getComments);
     }
 
     public function getPoints() {
@@ -140,7 +140,31 @@ sql;
         return $this->getPoints($cleanedInput['comment_id']);
 
         // Return the Insert ID
-        return $results->insert_id;
+        // return $results->insert_id;
+    }
+
+    public function getVotes($input) {
+
+        $cleanedInput = $this->cleanInput(
+            ['user_id'],
+            $input
+        );
+
+        if (is_string($cleanedInput)) return null;
+
+        $votes =<<<sql 
+            SELECT *
+            FROM 
+            man_point
+            WHERE user_id = {$cleanedInput['user_id']};
+sql;
+            $results = db::execute($votes);
+
+            return $this->getVotes($cleanedInput['user_id']);
+
+            // Return the Insert ID
+            // return $results->insert_id;
+
     }
 
 }
